ef4874221b151d34be6cba85be415c98bf2d5366
Jorge Neto <53627696+Jorge-Neto@users.noreply.github.com>
2022-04-25 10:18:41 -0300

    Implementando get de API externa na API Node

--- apione/src/index.js
+++ apione/src/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const requests = axios.create({
+  // Defina URL da sua API aqui
+  baseURL: 'http://www.omdbapi.com',
+  headers: { 'Content-Type': 'application/json' },
+});
 
 const app = express();
 const router = express.Router();
@@ -9,6 +16,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 app.get('/', (req, res) => { res.send('Hello World!'); });
+
+app.get('/movie/:title', async (req, res) => {
+    title = req.params.title;
+    await requests.get(`/?t=${title}&apikey=ccd090fe`).then(response => res.send(response.data));
+})
 
 app.use(router);
 app.listen(9001);
